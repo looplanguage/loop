34b80608d2dca9c7b5e35d1dce5bc3a30b36aa65
kanersps <12296463+kanersps@users.noreply.github.com>
2021-10-04 15:16:46 +0200

    feat: parse grouped expressions correctly

--- src/parser/expression/suffix.rs
+++ src/parser/expression/suffix.rs
@@ -1,4 +1,6 @@
+use crate::lexer::token::TokenType::RightParenthesis;
 use crate::parser::expression::Expression;
+use crate::parser::expression::Precedence::LOWEST;
 use crate::parser::Parser;
 
 #[derive(Debug)]
@@ -20,4 +22,15 @@ pub fn parse_suffix_expression(p: &mut Parser, expression: Expression) -> Expres
         operator,
         right: p.parse_expression(pre).unwrap()
     }))
+}
+
+pub fn parse_grouped_expression(p: &mut Parser) -> Expression {
+    p.lexer.next();
+    let exp = p.parse_expression(LOWEST);
+
+    if !p.lexer.next_is(RightParenthesis) {
+        p.add_error(format!("wrong token. expected=\")\". got=\"{}\"", p.lexer.current_token.clone().unwrap().literal))
+    }
+
+    return exp.unwrap()
 }
