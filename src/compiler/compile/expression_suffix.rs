8acfe1b3b5d08560e3ffd2b638b249235282023f
Kane Petra <kane.petra@looplang.org>
2021-10-05 22:11:11 +0200

    feat: add all math operations

--- src/compiler/compile/expression_suffix.rs
+++ src/compiler/compile/expression_suffix.rs
@@ -6,7 +6,14 @@ pub fn compile_expression_suffix(_compiler: &mut Compiler, _suffix: Suffix) -> O
     _compiler.compile_expression(_suffix.left);
     _compiler.compile_expression(_suffix.right);
 
-    _compiler.emit(OpCode::Add, vec![]);
+    match _suffix.operator.as_str() {
+        "+" => { _compiler.emit(OpCode::Add, vec![]); },
+        "-" => { _compiler.emit(OpCode::Minus, vec![]); },
+        "*" => { _compiler.emit(OpCode::Multiply, vec![]); },
+        "/" => { _compiler.emit(OpCode::Divide, vec![]); },
+        "%" => { _compiler.emit(OpCode::Modulo, vec![]); },
+        _ => { panic!(format!("unknown operator. got={}", _suffix.operator)) }
+    }
 
     None
 }
---
+++ src/vm/suffix.rs
@@ -0,0 +1,49 @@
+use crate::object::integer::Integer;
+use crate::object::Object;
+use crate::vm::VM;
+
+pub fn run_suffix_expression(vm: &mut VM, operator: &str) {
+    let right = vm.pop();
+    let left = vm.pop();
+
+    match operator {
+        "+" => {
+            if let Object::Integer(left_obj) = left {
+                if let Object::Integer(right_obj) = right {
+                    vm.push(Object::Integer(Integer { value: left_obj.value + right_obj.value }));
+                };
+            };
+        },
+        "*" => {
+            if let Object::Integer(left_obj) = left {
+                if let Object::Integer(right_obj) = right {
+                    vm.push(Object::Integer(Integer { value: left_obj.value * right_obj.value }));
+                };
+            };
+        },
+        "-" => {
+            if let Object::Integer(left_obj) = left {
+                if let Object::Integer(right_obj) = right {
+                    vm.push(Object::Integer(Integer { value: left_obj.value - right_obj.value }));
+                };
+            };
+        },
+        "%" => {
+            if let Object::Integer(left_obj) = left {
+                if let Object::Integer(right_obj) = right {
+                    vm.push(Object::Integer(Integer { value: left_obj.value % right_obj.value }));
+                };
+            };
+        },
+        "/" => {
+            if let Object::Integer(left_obj) = left {
+                if let Object::Integer(right_obj) = right {
+                    vm.push(Object::Integer(Integer { value: left_obj.value / right_obj.value }));
+                };
+            };
+        },
+        _ => {
+            panic!(format!("unknown operator {}", operator))
+        }
+    }
+}
