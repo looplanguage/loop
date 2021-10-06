ef73edb7d12513d03160542e982d1a42ec5458f7
Pennings,Wouter W.E.M <465288@student.fontys.nl>
2021-10-06 20:57:59 +0200

    feat: flags package for "cargo run"

---
+++ src/flags/mod.rs
@@ -0,0 +1,31 @@
+#[derive(PartialEq)]
+pub enum FlagTypes {
+    None,
+    Debug,
+}
+
+pub fn build_flags() -> Flags {
+    return Flags { flags: vec![] };
+}
+
+pub struct Flags {
+    pub flags: Vec<FlagTypes>,
+}
+
+impl Flags {
+    pub fn get_flag(string: &str) -> FlagTypes {
+        match string {
+            "--debug" | "-d" => FlagTypes::Debug,
+            &_ => FlagTypes::None,
+        }
+    }
+
+    pub fn parse_flags(&mut self, args: Vec<String>) {
+        for arg in args {
+            let flag = Flags::get_flag(arg.as_str());
+            if flag != FlagTypes::None {
+                self.flags.push(flag)
+            }
+        }
+    }
+}
