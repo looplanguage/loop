44f35c345c9be10e778c3d24f8167fed653eb46a
kanersps <12296463+kanersps@users.noreply.github.com>
2021-10-07 10:06:05 +0200

    fix: issue with block parsing

--- src/parser/statement/block.rs
+++ src/parser/statement/block.rs
@@ -12,6 +12,11 @@ pub fn parse_block(p: &mut Parser) -> Block {
     let mut statements: Vec<Statement> = Vec::new();
 
     while p.lexer.current_token.clone().unwrap().token != TokenType::RightBrace {
+        if p.cur_token_is(TokenType::Semicolon) {
+            p.lexer.next_token();
+            continue
+        }
+
         let stmt = p.parse_statement(p.lexer.current_token.clone().unwrap());
 
         if stmt.is_none() {
