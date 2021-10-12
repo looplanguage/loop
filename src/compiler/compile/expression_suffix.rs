946bb454e2ad1427dcc4dfeac0d4339476a7b3a3
Kanersps <kanepetra@gmail.com>
2021-10-12 20:20:51 +0200

    feat: don't allow division by zero

--- src/compiler/compile/expression_suffix.rs
+++ src/compiler/compile/expression_suffix.rs
@@ -1,8 +1,11 @@
 use crate::compiler::opcode::OpCode;
 use crate::compiler::Compiler;
 use crate::parser::expression::suffix::Suffix;
+use crate::parser::expression::Expression;
 
 pub fn compile_expression_suffix(_compiler: &mut Compiler, _suffix: Suffix) -> Option<String> {
+    let right = _suffix.right.clone();
+
     if _suffix.operator.as_str() == "<" {
         _compiler.compile_expression(_suffix.right);
         _compiler.compile_expression(_suffix.left);
@@ -22,6 +25,20 @@ pub fn compile_expression_suffix(_compiler: &mut Compiler, _suffix: Suffix) -> O
             _compiler.emit(OpCode::Multiply, vec![]);
         }
         "/" => {
+            match right {
+                Expression::Integer(integer) => {
+                    if integer.value == 0 {
+                        return Some("can not divide by 0".to_string());
+                    }
+                }
+                Expression::Float(float) => {
+                    if float.value == 0.0 {
+                        return Some("can not divide by 0".to_string());
+                    }
+                }
+                _ => {}
+            }
+
             _compiler.emit(OpCode::Divide, vec![]);
         }
         "%" => {
