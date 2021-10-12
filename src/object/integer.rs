24bbf9ce5df4d47b5c40eb2765624d9195477133
Kane Petra <kane.petra@looplang.org>
2021-10-12 20:04:35 +0200

    feat: support all expressions for floats

--- src/object/integer.rs
+++ src/object/integer.rs
@@ -1,8 +1,17 @@
+use crate::object::float::Float;
 use crate::object::ObjectTrait;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Integer {
     pub(crate) value: i64,
+}
+
+impl Integer {
+    pub fn to_float(&self) -> Float {
+        Float {
+            value: self.value as f64,
+        }
+    }
 }
 
 impl ObjectTrait for Integer {
