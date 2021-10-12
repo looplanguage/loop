59b68ad5231af88ae970b706bbefc9f5ad56b530
kanersps <12296463+kanersps@users.noreply.github.com>
2021-10-12 13:47:34 +0200

    format: format files

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -15,11 +15,11 @@ use crate::parser::expression::suffix::{parse_grouped_expression, parse_suffix_e
 use crate::parser::expression::{get_precedence, Expression, Precedence};
 use crate::parser::program::{Node, Program};
 use crate::parser::statement::assign::parse_variable_assignment;
+use crate::parser::statement::block::parse_block_statement;
 use crate::parser::statement::expression::parse_expression_statement;
 use crate::parser::statement::return_statement::parse_return_statement;
 use crate::parser::statement::Statement;
 use std::collections::HashMap;
-use crate::parser::statement::block::parse_block_statement;
 
 use self::statement::variable::parse_variable_declaration;
 
