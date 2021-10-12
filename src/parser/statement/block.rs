00af8c84ad9144d605fe4079128cb2ce17bbd14b
kanersps <12296463+kanersps@users.noreply.github.com>
2021-10-12 09:10:52 +0200

    tweak: throw exceptions correctly

--- src/parser/statement/block.rs
+++ src/parser/statement/block.rs
@@ -11,7 +11,7 @@ pub struct Block {
 pub fn parse_block(p: &mut Parser) -> Block {
     let mut statements: Vec<Statement> = Vec::new();
 
-    while p.lexer.current_token.clone().unwrap().token != TokenType::RightBrace {
+    while p.lexer.current_token.clone().unwrap().token != TokenType::RightBrace && p.lexer.current_token.clone().unwrap().token != TokenType::Eof {
         let stmt = p.parse_statement(p.lexer.current_token.clone().unwrap());
 
         if let Some(Node::Statement(statement)) = stmt {
