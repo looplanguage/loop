d7e175ab001db2aefac8fabf364d41ef061d520f
kanersps <12296463+kanersps@users.noreply.github.com>
2021-10-13 14:30:01 +0200

    feat: implement sentry

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,23 @@ pub mod parser;
 mod repl;
 mod vm;
 
+#[cfg(debug_assertions)]
+fn get_build() -> String {
+    "development".to_string()
+}
+
+#[cfg(not(debug_assertions))]
+fn get_build() -> String {
+    "release".to_string()
+}
+
 fn main() {
+    let _guard = sentry::init(("https://d071f32c72f44690a1a7f9821cd15ace@o1037493.ingest.sentry.io/6005454", sentry::ClientOptions {
+        release: sentry::release_name!(),
+        environment: Some(get_build().into()),
+        ..Default::default()
+    }));
+
     let flags = get_flags();
 
     if let Some(file) = flags.file {
