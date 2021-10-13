2338772cd90f6158d380515a4080d5772d8eb09d
Pennings,Wouter W.E.M <p.pennings@student.fontys.nl>
2021-10-13 12:23:17 +0200

    tweak: test helper moudle

--- src/lexer/test_helper.rs
+++ src/lexer/test_helper.rs
@@ -1,8 +1,11 @@
-use crate::lexer::token::{Token, TokenType};
+#[cfg(test)]
+mod test_helper {
+    use crate::lexer::token::{Token, TokenType};
 
-pub fn generate_token(literal: &str, token_type: TokenType) -> Token {
-    Token {
-        token: token_type,
-        literal: literal.to_string(),
+    pub fn generate_token(literal: &str, token_type: TokenType) -> Token {
+        Token {
+            token: token_type,
+            literal: literal.to_string(),
+        }
     }
 }
