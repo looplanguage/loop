1ce2a1d2dbb34781fedb31d163362e08b75b71f7
Kanersps <kanepetra@gmail.com>
2021-10-23 14:42:05 +0200

    fix: parser issue

--- src/parser/expression/index.rs
+++ src/parser/expression/index.rs
@@ -1,3 +1,5 @@
+use crate::parser::expression::function::parse_call;
+use crate::parser::expression::identifier::parse_identifier;
 use crate::parser::expression::{Expression, Precedence};
 use crate::parser::program::Node;
 use crate::parser::Parser;
@@ -11,15 +13,19 @@ pub struct Index {
 pub fn parse_index_expression(p: &mut Parser, left: Expression) -> Option<Node> {
     p.lexer.next_token();
 
-    let exp = p.parse_expression(Precedence::Lowest);
+    let identifier = parse_identifier(p);
+    if let Node::Expression(ident_exp) = identifier.unwrap() {
+        p.lexer.next_token();
+        let exp = parse_call(p, ident_exp);
 
-    exp.as_ref()?;
+        exp.as_ref()?;
 
-    if let Node::Expression(val) = exp.unwrap() {
-        return Some(Node::Expression(Expression::Index(Box::from(Index {
-            left,
-            right: val,
-        }))));
+        if let Node::Expression(val) = exp.unwrap() {
+            return Some(Node::Expression(Expression::Index(Box::from(Index {
+                left,
+                right: val,
+            }))));
+        }
     }
 
     None
