d90aaf430d1437039b66594adeddc80383431fb3
WouterPennigs <wouterpennings@gmail.com>
2021-10-23 18:04:03 +0200

    feat: floats

--- src/parser/expression/float.rs
+++ src/parser/expression/float.rs
@@ -7,7 +7,7 @@ pub struct Float {
     pub value: f64,
 }
 
-pub fn parse_float_literal(p: &mut Parser, left: Expression) -> Option<Node> {
+/*pub fn parse_float_literal(p: &mut Parser, left: Expression) -> Option<Node> {
     let integer = match left {
         Expression::Integer(integer) => integer,
         _ => return None,
@@ -37,5 +37,37 @@ pub fn parse_float_literal(p: &mut Parser, left: Expression) -> Option<Node> {
 
     Some(Node::Expression(Expression::Float(Float {
         value: value.unwrap(),
+    })))
+}*/
+
+pub fn parse_float_literal(p: &mut Parser) -> Option<Node> {
+    let value = p
+        .lexer
+        .current_token
+        .clone()
+        .unwrap()
+        .literal
+        .parse::<f64>()
+        .unwrap();
+
+    let exp = Expression::Float(Float { value });
+
+    Some(Node::Expression(exp))
+}
+
+pub fn parse_minus_float(p: &mut Parser) -> Option<Node> {
+    p.lexer.next_token();
+
+    let value = p
+        .lexer
+        .current_token
+        .clone()
+        .unwrap()
+        .literal
+        .parse::<f64>()
+        .unwrap();
+
+    Some(Node::Expression(Expression::Float(Float {
+        value: -value,
     })))
 }
