53e58e50d7d0ac83c0f491bfd818d7dea31a52d8
WouterPennigs <wouterpennings@gmail.com>
2021-10-24 22:14:41 +0200

    fix: removed unused imports

--- src/parser/expression/float.rs
+++ src/parser/expression/float.rs
@@ -1,7 +1,3 @@
-use crate::parser::expression::Expression;
-use crate::parser::program::Node;
-use crate::parser::Parser;
-
 #[derive(Debug, PartialEq, Clone)]
 pub struct Float {
     pub value: f64,
