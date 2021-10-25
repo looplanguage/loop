9f37cc99a0b853329cb9d595f22a7bc654de8e17
Kane Petra <kane.petra@looplang.org>
2021-10-25 23:19:01 +0200

    feat: compile arrays

--- src/compiler/compile/expression_array.rs
+++ src/compiler/compile/expression_array.rs
@@ -1,3 +1,4 @@
+use crate::compiler::opcode::OpCode;
 use crate::compiler::Compiler;
 use crate::lib::exception::compiler::CompilerException;
 use crate::lib::object::Object;
@@ -5,10 +6,13 @@ use crate::parser::expression::array;
 use crate::parser::expression::array::Array;
 
 pub fn compile_expression_array(compiler: &mut Compiler, arr: Array) -> Option<CompilerException> {
-    /*
-    let ct = compiler.add_constant(Object::Array(array::Array { values: arr.values }));
-    compiler.emit(OpCode::Constant, vec![ct]);
-    */
+    let array_length = arr.values.len() as u32;
+
+    for value in arr.values {
+        compiler.compile_expression(*value.expression);
+    }
+
+    compiler.emit(OpCode::Array, vec![array_length]);
 
     None
 }
