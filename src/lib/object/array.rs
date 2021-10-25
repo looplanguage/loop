4e26aecc1f5f3affff1531a189eed6e795732c5d
Kane Petra <kane.petra@looplang.org>
2021-10-25 23:01:50 +0200

    tweak: fix errors

--- src/lib/object/array.rs
+++ src/lib/object/array.rs
@@ -1,6 +1,6 @@
 use crate::lib::object::{Object, ObjectTrait};
 
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Array {
     pub(crate) values: Vec<Object>,
 }
@@ -9,7 +9,7 @@ impl ObjectTrait for Array {
     fn inspect(&self) -> String {
         let mut arr = String::from("[");
 
-        for value in self.values {
+        for value in &self.values {
             arr.push(format!("{}, ", value.inspect()).parse().unwrap());
         }
 
