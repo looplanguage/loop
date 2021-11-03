8c7f0b0a1c7fecc968355b58a860a3a0936377ca
Kanersps <kanepetra@gmail.com>
2021-11-03 19:17:39 +0100

    tweak: mark unused variable

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ pub mod parser;
 mod vm;
 
 fn main() {
-    let config = match load_config() {
+    let _config = match load_config() {
         LoadType::FirstRun(cfg) => {
             println!("This is your first time running Loop! (Or your config was re-generated)");
             println!("Configuration file is at: ");
