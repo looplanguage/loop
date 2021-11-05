4cd9d049e0a1314a3a64e25fc2a937e31c6169f0
Kanersps <12296463+kanersps@users.noreply.github.com>
2021-11-05 12:53:23 +0100

    tweak: add back all extension methods

--- src/lib/object/extension_method.rs
+++ src/lib/object/extension_method.rs
@@ -1,10 +1,10 @@
-use std::cell::RefCell;
-use std::rc::Rc;
 use crate::lib::exception::vm::VMException;
 use crate::lib::object::integer::Integer;
 use crate::lib::object::null::Null;
-use crate::lib::object::{boolean, Object};
 use crate::lib::object::string::LoopString;
+use crate::lib::object::{boolean, Object};
+use std::cell::RefCell;
+use std::rc::Rc;
 
 pub type EvalResult = Result<Object, VMException>;
 pub type BuiltinFunction = fn(Rc<RefCell<Object>>, Vec<Object>) -> EvalResult;
@@ -23,7 +23,14 @@ macro_rules! extension {
     };
 }
 
-pub const EXTENSION_METHODS: &[Builtin] = &[extension!(to_string), extension!(to_int), extension!(add)];
+pub const EXTENSION_METHODS: &[Builtin] = &[
+    extension!(to_string),
+    extension!(to_int),
+    extension!(add),
+    extension!(remove),
+    extension!(slice),
+    extension!(length),
+];
 
 pub fn lookup(name: &str) -> Option<u32> {
     if name == "null" {
@@ -43,17 +50,15 @@ pub fn lookup(name: &str) -> Option<u32> {
 
 fn to_string(extending: Rc<RefCell<Object>>, arguments: Vec<Object>) -> EvalResult {
     return match &*extending.as_ref().borrow() {
-        Object::Integer(integer) => {
-            Ok(Object::String(LoopString {
-                value: integer.value.to_string()
-            }))
-        }
-        _ => {
-            Ok(Object::Null(Null {}))
-        }
-    }
+        Object::Integer(integer) => Ok(Object::String(LoopString {
+            value: integer.value.to_string(),
+        })),
+        _ => Err(VMException::IncorrectType(format!(
+            "incorrect type. got=\"{:?}\"",
+            extending
+        ))),
+    };
 }
-
 
 fn to_int(extending: Rc<RefCell<Object>>, arguments: Vec<Object>) -> EvalResult {
     return match &*extending.as_ref().borrow() {
@@ -61,26 +66,133 @@ fn to_int(extending: Rc<RefCell<Object>>, arguments: Vec<Object>) -> EvalResult 
             let parsed = string.value.parse::<i64>();
 
             if parsed.is_err() {
-                return Err(VMException::CannotParseInt(format!("unable to parse. got=\"{:?}\"", extending)))
+                return Err(VMException::CannotParseInt(format!(
+                    "unable to parse. got=\"{:?}\"",
+                    extending
+                )));
             }
 
             Ok(Object::Integer(Integer {
-                value: parsed.unwrap()
+                value: parsed.unwrap(),
             }))
         }
-        _ => {
-            Ok(Object::Null(Null {}))
+        _ => Err(VMException::IncorrectType(format!(
+            "incorrect type. got=\"{:?}\"",
+            extending
+        ))),
+    };
+}
+
+pub fn add(extending: Rc<RefCell<Object>>, arguments: Vec<Object>) -> EvalResult {
+    match &mut *extending.as_ref().borrow_mut() {
+        Object::Array(ref mut arr) => {
+            for arg in arguments {
+                arr.values.push(Rc::from(RefCell::from(arg)));
+            }
+
+            Ok(Object::Boolean(boolean::Boolean { value: true }))
         }
+        _ => Err(VMException::IncorrectType(format!(
+            "incorrect type. got=\"{:?}\"",
+            extending
+        ))),
     }
 }
 
-// 0: add(obj: Object)
-pub fn add(extending: Rc<RefCell<Object>>, arguments: Vec<Object>) -> EvalResult {
-    if let Object::Array(ref mut arr) = &mut *extending.as_ref().borrow_mut() {
-        for arg in arguments {
-            arr.values.push(Rc::from(RefCell::from(arg)));
-        }
-    }
+pub fn remove(extending: Rc<RefCell<Object>>, arguments: Vec<Object>) -> EvalResult {
+    match &mut *extending.as_ref().borrow_mut() {
+        Object::Array(ref mut arr) => {
+            if arguments.len() != 1 {
+                return Err(VMException::IncorrectArgumentCount(
+                    1,
+                    arguments.len() as i32,
+                ));
+            }
 
-    Ok(Object::Boolean(boolean::Boolean { value: true }))
+            let mut removed: Object = Object::Boolean(boolean::Boolean { value: false });
+            if let Object::Integer(integer) = arguments[0] {
+                if arr.values.len() > integer.value as usize {
+                    removed = arr
+                        .values
+                        .remove(integer.value as usize)
+                        .as_ref()
+                        .borrow()
+                        .clone();
+                } else {
+                    return Err(VMException::EmptyArray);
+                }
+            } else {
+                return Err(VMException::IncorrectType(format!(
+                    "wrong type. expected=\"Integer\". got=\"{:?}\"",
+                    arguments[0]
+                )));
+            }
+
+            return Ok(removed);
+        }
+        _ => Err(VMException::IncorrectType(format!(
+            "incorrect type. got=\"{:?}\"",
+            extending
+        ))),
+    }
+}
+
+pub fn slice(extending: Rc<RefCell<Object>>, arguments: Vec<Object>) -> EvalResult {
+    match &mut *extending.as_ref().borrow_mut() {
+        Object::Array(ref mut arr) => {
+            if arguments.len() != 2 {
+                return Err(VMException::IncorrectArgumentCount(
+                    2,
+                    arguments.len() as i32,
+                ));
+            }
+
+            let mut removed: Object = Object::Boolean(boolean::Boolean { value: true });
+            if let Object::Integer(start) = arguments[0] {
+                if let Object::Integer(end) = arguments[1] {
+                    // 3, 0, 1
+                    if start.value <= end.value
+                        && arr.values.len() - 1 >= start.value as usize
+                        && arr.values.len() - 1 >= end.value as usize
+                    {
+                        arr.values =
+                            arr.values[start.value as usize..(end.value + 1) as usize].to_owned()
+                    } else {
+                        return Err(VMException::EmptyArray);
+                    }
+                } else {
+                    return Err(VMException::IncorrectType(format!(
+                        "wrong type. expected=\"Integer\". got=\"{:?}\"",
+                        arguments[0]
+                    )));
+                }
+            } else {
+                return Err(VMException::IncorrectType(format!(
+                    "wrong type. expected=\"Integer\". got=\"{:?}\"",
+                    arguments[0]
+                )));
+            }
+
+            return Ok(removed);
+        }
+        _ => Err(VMException::IncorrectType(format!(
+            "incorrect type. got=\"{:?}\"",
+            extending
+        ))),
+    }
+}
+
+fn length(extending: Rc<RefCell<Object>>, arguments: Vec<Object>) -> EvalResult {
+    return match &*extending.as_ref().borrow() {
+        Object::Array(arr) => Ok(Object::Integer(Integer {
+            value: arr.values.len() as i64,
+        })),
+        Object::String(str) => Ok(Object::Integer(Integer {
+            value: str.value.len() as i64,
+        })),
+        _ => Err(VMException::IncorrectType(format!(
+            "incorrect type. got=\"{:?}\"",
+            extending
+        ))),
+    };
 }
