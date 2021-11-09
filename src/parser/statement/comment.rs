0c2612c745fc1ef5b5ecfb92f94d38da767d0b1a
WouterPennings <wouterpennings@gmail.com>
2021-11-09 15:39:43 +0100

    fix: removed print

--- src/parser/statement/comment.rs
+++ src/parser/statement/comment.rs
@@ -1,7 +1,6 @@
 use crate::parser::Parser;
 use crate::parser::program::Node;
 
-pub fn parse_comment(p: &mut Parser) -> Option<Node>{
-    println!("input: {}", p.lexer.current_token.as_ref().unwrap().literal);
+pub fn parse_comment(p: &mut Parser) -> Option<Node> {
     None
 }
