54dec5713c47b4b4059ecefb69a8a9a50a0ba587
Kane Petra <kane.petra@looplang.org>
2021-11-09 20:46:27 +0100

    tweak(parser): additional errors for hashmap parsing

--- src/parser/expression/hashmap.rs
+++ src/parser/expression/hashmap.rs
@@ -1,13 +1,13 @@
-use std::collections::HashMap;
-use std::hash::Hash;
 use crate::lexer::token::TokenType;
 use crate::parser::expression::boolean::Boolean;
-use crate::parser::expression::Expression;
 use crate::parser::expression::integer::Integer;
-use crate::parser::expression::Precedence::Lowest;
 use crate::parser::expression::string::LoopString;
-use crate::parser::Parser;
+use crate::parser::expression::Expression;
+use crate::parser::expression::Precedence::Lowest;
 use crate::parser::program::Node;
+use crate::parser::Parser;
+use std::collections::HashMap;
+use std::hash::Hash;
 
 #[derive(Debug, PartialEq, Clone, Eq, Hash)]
 pub enum HashableExpression {
@@ -18,21 +18,29 @@ pub enum HashableExpression {
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Hashmap {
-    pub(crate) values: HashMap<HashableExpression, Expression>
+    pub(crate) values: HashMap<HashableExpression, Expression>,
 }
 
 pub fn parse_expression_hashmap(p: &mut Parser) -> Option<Node> {
     p.lexer.next_token();
+
     let mut values: HashMap<HashableExpression, Expression> = HashMap::new();
 
     while p.lexer.current_token.clone().unwrap().token != TokenType::RightBrace
         && p.lexer.current_token.clone().unwrap().token != TokenType::Eof
     {
+        if p.lexer.current_token.clone().unwrap().token == TokenType::Comma {
+            p.lexer.next_token();
+        }
+
         let key_exp = p.parse_expression(Lowest);
 
         if let Some(Node::Expression(key)) = key_exp {
             if !p.lexer.next_is(TokenType::Colon) {
-                p.add_error(format!("wrong token. expected=\"Colon\". got=\"{:?}\"", p.lexer.peek_token.clone().unwrap().token));
+                p.add_error(format!(
+                    "wrong token. expected=\"Colon\". got=\"{:?}\"",
+                    p.lexer.peek_token.clone().unwrap().token
+                ));
                 return None;
             }
 
@@ -45,20 +53,26 @@ pub fn parse_expression_hashmap(p: &mut Parser) -> Option<Node> {
                 if let Some(hash) = hashable {
                     values.insert(hash, val);
                 } else {
-                    p.add_error(format!("type is not hashable therefore can not be used as key. got=\"{:?}\"", key));
+                    p.add_error(format!(
+                        "type is not hashable therefore can not be used as key. got=\"{:?}\"",
+                        key
+                    ));
                     return None;
                 }
             }
         }
 
         p.lexer.next_token();
-
-        if p.lexer.current_token.clone().unwrap().token == TokenType::Comma {
-            p.lexer.next_token();
-        }
     }
 
-    Some(Node::Expression(Expression::Hashmap(Hashmap {
-        values
-    })))
+    if p.lexer.current_token.clone().unwrap().token != TokenType::RightBrace {
+        p.add_error(format!(
+            "wrong token. expected=\"RightBrace\". got=\"{:?}\"",
+            p.lexer.current_token.clone().unwrap().token
+        ));
+
+        return None;
+    }
+
+    Some(Node::Expression(Expression::Hashmap(Hashmap { values })))
 }
