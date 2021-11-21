8148f0d9525607ace9fa3766cf6c4ddb962929d6
WouterPennings <wouterpennings@gmail.com>
2021-11-22 00:02:17 +0100

    feat: compile enum

--- src/compiler/compile/statement_enum.rs
+++ src/compiler/compile/statement_enum.rs
@@ -7,5 +7,13 @@ pub fn compile_enum_statement(
     compiler: &mut Compiler,
     e: EnumStatement,
 ) -> Option<CompilerException> {
+    let enum_length = e.identifiers.len() as u32;
+
+    for ident in e.identifiers {
+        compiler.compile_expression_identifier(compiler, ident);
+    }
+
+    compiler.emit(OpCode::Enum, vec![enum_length]);
+
     None
 }
