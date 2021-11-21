86125f1411600879e065a4497acd74e2f7728b05
WouterPennings <wouterpennings@gmail.com>
2021-11-22 00:04:16 +0100

    fix: added different indentifier function

--- src/compiler/compile/statement_enum.rs
+++ src/compiler/compile/statement_enum.rs
@@ -2,6 +2,7 @@ use crate::compiler::opcode::OpCode;
 use crate::compiler::Compiler;
 use crate::lib::exception::compiler::CompilerException;
 use crate::parser::statement::enum_statement::EnumStatement;
+use crate::compiler::compile::expression_identifier::compile_expression_identifier;
 
 pub fn compile_enum_statement(
     compiler: &mut Compiler,
@@ -10,7 +11,7 @@ pub fn compile_enum_statement(
     let enum_length = e.identifiers.len() as u32;
 
     for ident in e.identifiers {
-        compiler.compile_expression_identifier(compiler, ident);
+        compile_expression_identifier(compiler, ident);
     }
 
     compiler.emit(OpCode::Enum, vec![enum_length]);
