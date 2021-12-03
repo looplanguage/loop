621efe172320efad801ecf2614660fe4e9c791b0
Kane Petra <kane.petra@looplang.org>
2021-12-03 13:12:44 +0100

    feat: simple format function
    Only with %a placeholder, a = anything

--- src/lib/object/builtin.rs
+++ src/lib/object/builtin.rs
@@ -1,9 +1,11 @@
+use std::borrow::Borrow;
 use crate::lib::exception::vm::VMException;
 use crate::lib::object::integer::Integer;
 use crate::lib::object::null::Null;
 use crate::lib::object::Object;
 use std::cell::RefCell;
 use std::rc::Rc;
+use crate::lib::object::string::LoopString;
 
 pub type EvalResult = Result<Object, VMException>;
 pub type BuiltinFunction = fn(Vec<Rc<RefCell<Object>>>) -> EvalResult;
@@ -22,7 +24,7 @@ macro_rules! builtin {
     };
 }
 
-pub const BUILTINS: &[Builtin] = &[builtin!(len), builtin!(print), builtin!(println)];
+pub const BUILTINS: &[Builtin] = &[builtin!(len), builtin!(print), builtin!(println), builtin!(format)];
 
 /*
 pub fn lookup(name: &str) -> Option<Object> {
@@ -83,6 +85,49 @@ fn len(arguments: Vec<Rc<RefCell<Object>>>) -> EvalResult {
             &arguments[0]
         ))),
     }
+}
+
+fn format(arguments: Vec<Rc<RefCell<Object>>>) -> EvalResult {
+    if arguments.len() < 2 {
+        return Err(VMException::IncorrectArgumentCount(
+            2 as i32,
+            arguments.len() as i32,
+        ));
+    } else {
+        if let Object::String(string) = &*arguments[0].as_ref().borrow() {
+            let mut copy = string.value.clone();
+
+            for i in 1..arguments.len() {
+                match &*arguments[i].as_ref().borrow() {
+                    Object::Integer(int) => copy = copy.replacen("%a", int.value.to_string().as_str(), 1),
+                    Object::Boolean(boolean) => copy = copy.replacen("%a", boolean.value.to_string().as_str(), 1),
+                    Object::Null(_) => copy = copy.replacen("%a", "null", 1),
+                    Object::Float(float) => copy = copy.replacen("%a", float.value.to_string().as_str(), 1),
+                    Object::String(str) => copy = copy.replacen("%a", str.value.to_string().as_str(), 1),
+                    _ => {
+                        return Err(VMException::IncorrectType(format!(
+                            "incorrect type for function 'format'. got=\"{:?}\"",
+                            &arguments[i]
+                        )));
+                    }
+                }
+            }
+
+            return Ok(Object::String(LoopString {
+                value: copy
+            }))
+        } else {
+            return Err(VMException::IncorrectType(format!(
+                "incorrect type for function 'format'. got=\"{:?}\"",
+                &arguments[0]
+            )));
+        }
+    }
+
+    return Err(VMException::IncorrectArgumentCount(
+        2 as i32,
+        arguments.len() as i32,
+    ));
 }
 
 fn check_length(args: Vec<Rc<RefCell<Object>>>, required_length: usize) -> EvalResult {
