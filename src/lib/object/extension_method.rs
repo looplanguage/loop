029965a428c83f16c22f095d13dd50c004ee8354
WouterPennings <wouterpennings@gmail.com>
2021-12-07 23:57:58 +0100

    feat: to.int for boolean

--- src/lib/object/extension_method.rs
+++ src/lib/object/extension_method.rs
@@ -72,6 +72,17 @@ fn to_int(extending: Rc<RefCell<Object>>, _arguments: Vec<Object>) -> EvalResult
                 value: parsed.unwrap(),
             }))
         }
+        Object::Boolean(boolean) => {
+            if boolean.value == true {
+                Ok(Object::Integer(Integer {
+                    value: 1,
+                }))
+            } else {
+                Ok(Object::Integer(Integer {
+                    value: 0,
+                }))
+            }
+        }
         _ => Err(VMException::IncorrectType(format!(
             "incorrect type. got=\"{:?}\"",
             extending
