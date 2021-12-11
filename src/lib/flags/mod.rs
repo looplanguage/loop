c60833c9e1c428e4e4ba3c7580037acbfda66571
WouterPennings <wouterpennings@gmail.com>
2021-12-11 16:18:29 +0100

    feat: parameters with the arguments

--- src/lib/flags/mod.rs
+++ src/lib/flags/mod.rs
@@ -1,8 +1,10 @@
 #[derive(PartialEq)]
 pub enum FlagTypes {
+    None,
     Debug,
     Benchmark,
     Jit,
+    Optimize, // There are no optimizations yet, this is for the near future
 }
 
 pub fn build_flags() -> Flags {
@@ -18,11 +20,64 @@ pub struct Flags {
 }
 
 impl Flags {
+    // ToDo: Make Loop crash in an elegant way instead of calling the "panic" function of Rust
+    // ToDo: This is regarding the whole implementation of Flags.
+
     fn get_flag(string: &str) -> Result<FlagTypes, String> {
-        match string {
-            "--debug" | "-d" => Ok(FlagTypes::Debug),
-            "--benchmark" | "-b" => Ok(FlagTypes::Benchmark),
-            "--jit" | "-j" => Ok(FlagTypes::Jit),
+        let x: Vec<&str> = string.split('=').collect();
+
+        if x.len() > 2 {
+            return Err(format!(
+                "Found \"{}\" arguments, expected a max of one",
+                x.len() - 1
+            ));
+        }
+
+        match x[0] {
+            "--debug" | "-d" => {
+                if x[1] == "true" {
+                    return Ok(FlagTypes::Debug);
+                } else if x[1] == "false" {
+                    return Ok(FlagTypes::None);
+                }
+                return Err(format!(
+                    "Found parameter: \"{}\", which wasn't expected, or isn't valid in this context",
+                    x[1]
+                ));
+            }
+            "--benchmark" | "-b" => {
+                if x[1] == "true" {
+                    return Ok(FlagTypes::Benchmark);
+                } else if x[1] == "false" {
+                    return Ok(FlagTypes::None);
+                }
+                return Err(format!(
+                    "Found parameter: \"{}\", which wasn't expected, or isn't valid in this context",
+                    x[1]
+                ));
+            }
+            "--jit" | "-j" => {
+                if x[1] == "true" {
+                    return Ok(FlagTypes::Jit);
+                } else if x[1] == "false" {
+                    return Ok(FlagTypes::None);
+                }
+                return Err(format!(
+                    "Found parameter: \"{}\", which wasn't expected, or isn't valid in this context",
+                    x[1]
+                ));
+            }
+            "--optimize" | "-o" => {
+                if x[1] == "true" {
+                    return Ok(FlagTypes::Jit);
+                } else if x[1] == "false" {
+                    return Ok(FlagTypes::None);
+                }
+                return Err(format!(
+                    "Found parameter: \"{}\", which wasn't expected, or isn't valid in this context",
+                    x[1]
+                ));
+            }
             &_ => Err(format!(
                 "Found argument: \"{}\", which wasn't expected, or isn't valid in this context",
                 string
@@ -38,7 +93,6 @@ impl Flags {
             match flag {
                 Ok(e) => self.flags.push(e),
                 Err(e) => {
-                    // ToDo: Make Loop crash in an elegant way instead of calling the "panic" function of Rust
                     panic!("{}", e);
                 }
             }
