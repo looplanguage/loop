6ab9f68fdfc9b28aa1e5622ca541e216fe909515
WouterPennings <wouterpennings@gmail.com>
2022-01-26 01:40:59 +0100

    feat: parser exception thrower

--- src/lib/exception/parser.rs
+++ src/lib/exception/parser.rs
@@ -5,6 +5,7 @@ use std::process;
 // ToDo: Give some context with the error.
 // ToDo: Give file in which error occurred.
 pub struct ParserException {
+    error_line: String,
     expected: Token,
     got: Token,
     line: i32,
@@ -13,31 +14,30 @@ pub struct ParserException {
 }
 
 impl ParserException {
+    #[rustfmt::skip]
     pub fn throw_exception(&mut self) {
-        let type_exc = "SyntaxError";
-        let stripe = "==========================================";
+        println!("==========================================================");
+        println!("{}", format!("SyntaxError [{}:{}] -->", self.line, self.column).bright_red());
+        println!("    {}: {}", "Line".bright_blue(),format!("{}\n", self.error_line.as_str()).bright_white());
+        println!("    {}: {}", "Message".bright_blue(), format!("Expected: '{}', but got: '{}'\n", self.expected.literal, self.got.literal).bright_white());
 
-        let title = format!(
-            "{} [{}:{}]\n\n",
-            type_exc.bright_red(),
-            self.line,
-            self.column
-        );
-        let error = format!(
-            "   Expected: \'{}\', but got: \'{}\'\n",
-            self.expected.literal, self.got.literal
-        );
-
-        let mut note: String = "".to_string();
-        if self.extra_message.is_some() {
-            note = format!("    {}\n", self.extra_message.unwrap())
+        if self.extra_message.is_none() {
+            process::exit(0);
         }
+        let message = self.add_identation(8, self.extra_message.as_ref().unwrap().to_string());
+        println!("    {}:", "Note".bright_blue());
+        println!("        {}", message.bright_white());
+    }
 
-        println!(stripe);
-        println!(title);
-        println!(error);
-        println!(note);
-        println!(stripe);
-        process::exit(0)
+    fn add_identation(&mut self, spaces: i32, text: String) -> String {
+        let insert = " ".repeat(spaces as usize);
+        let mut new_text: String = String::from("");
+        for char in text.chars() {
+            if char == 'o' {
+                new_text += &*insert;
+            }
+            new_text.push(char);
+        }
+        return new_text.clone();
     }
 }
