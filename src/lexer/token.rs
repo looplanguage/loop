27777c81b5dc1f7ea8fe91c1d816c206ca8a592f
WouterPennings <wouterpennings@gmail.com>
2022-03-14 21:57:02 +0100

    feat(lexer): Token comments

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -6,58 +6,112 @@ pub struct Token {
 
 #[derive(PartialEq, Clone, Debug, AsRefStr, Copy, Eq, Hash)]
 pub enum TokenType {
+    /// Variable name
     Identifier,
+    /// Example: 'var'
     VariableDeclaration,
-    Assign,
+    /// Example: Positive and negative number without decimal
     Integer,
+    /// Example: Positive number with decimal
     Float,
-    Semicolon,
-    Plus,
-    Minus,
+    /// Example: Negative number with decimal
     MinusFloat,
+    /// Example:  ';'
+    Semicolon,
+    /// Example: '+'
+    Plus,
+    /// Example: '-'
+    Minus,
+    /// Example: '*'
     Multiply,
+    /// Example: '='
+    Assign,
+    /// Example: '/'
     Divide,
+    /// Example: '%'
     Modulo,
+    /// Example: '^'
     Power,
-    Equals,
-    NotEquals,
+    /// Example: '@'
     AtSign,
+    /// Example: '('
     LeftParenthesis,
+    /// Example: ')'
     RightParenthesis,
+    /// Example: '&'
     AndSign,
+    /// Example: '!'
     InvertSign,
+    /// Example: '#'
     HashSign,
+    /// Example: '$'
     DollarSign,
+    /// Example: '>'
     GreaterThan,
+    /// Example: '<'
     LessThan,
-    GreaterThanOrEquals,
-    LessThanOrEquals,
-    Unknown,
-    True,
-    False,
-    Function,
-    If,
-    Else,
-    Import,
-    Export,
-    And,
-    Or,
+    /// Example: ','
     Comma,
+    /// Example: '{'
     LeftBrace,
+    /// Example: '}'
     RightBrace,
-    Null,
-    Return,
+    /// Example: '.'
     Dot,
+    /// Example: '['
     LeftBracket,
+    /// Example: ']'
     RightBracket,
-    String,
-    As,
-    For,
-    From,
-    To,
-    In,
+    /// Example: ':'
     Colon,
+    /// Example: '=='
+    Equals,
+    /// Example: '!='
+    NotEquals,
+    /// Example: '>='
+    GreaterThanOrEquals,
+    /// Example: '<='
+    LessThanOrEquals,
+    /// Example: 'true' value
+    True,
+    /// Example: 'false' value
+    False,
+    /// Example: 'fn'
+    Function,
+    /// Example: 'if'
+    If,
+    /// Example: 'else'
+    Else,
+    /// Example: 'import'
+    Import,
+    /// Example: 'export'
+    Export,
+    /// Example: '&&' and 'and'
+    And,
+    /// Example: '||' and 'or'
+    Or,
+    /// Example: 'null' value
+    Null,
+    /// Example: 'return'
+    Return,
+    /// Description: A piece of text
+    ///
+    /// Example: "I am a piece of text"
+    String,
+    /// Example: 'as'
+    As,
+    /// Example: 'for'
+    For,
+    /// Example: 'from'
+    From,
+    /// Example: 'to'
+    To,
+    /// Example: 'in'
+    In,
+    /// Example: 'break'
     Break,
+    Unknown,
+    /// Indicates that there are no more tokens
     Eof,
 }
 
