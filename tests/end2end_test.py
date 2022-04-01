ebb825d44891c163c21472b35f8cbdad147f57e4
WouterPennings <wouterpennings@gmail.com>
2022-04-01 19:54:07 +0200

    tweak: fixed to var names to convention

--- tests/end2end_test.py
+++ tests/end2end_test.py
@@ -1,21 +1,21 @@
 # Imports the test library
-from testlib import RunTests, AddTest
+from testlib import run_tests, add_test
 
 def main():
     # Add your tests right here.
     # AddTest( FILE LOCATION OF LOOP FILE, EXPECTED ANSWER )
     # NOTE: The expected answer always needs to be a string
-    AddTest("test_comments.loop", "3")
-    AddTest("test_import_export.loop", "8")
+    add_test("test_comments.loop", "3")
+    add_test("test_import_export.loop", "8")
 
     # output, is the generated report of all the tests
-    # hasFailed, a boolean:
+    # has_failed, a boolean:
     #   > True: One or more test have failed.
     #   > False: All tests passed
-    output, hasFailed = RunTests()
+    output, has_failed = run_tests()
 
-    # Asserts if 'hasFailed' is false, otherwise prints a red failure message
-    assert not hasFailed, output + "\033[91mEnd2End Tests Have Failed...\033[0m"
+    # Asserts if 'has_failed' is false, otherwise prints a red failure message
+    assert not has_failed, output + "\033[91mEnd2End Tests Have Failed...\033[0m"
 
     # Prints a green success message
     print(output + "\033[92mEnd2End Tests Have Succeeded...\033[0m")
--- tests/testlib.py
+++ tests/testlib.py
@@ -12,38 +12,38 @@ class Test:
     answer: str
 
 tests = []
-def AddTest(file_loc, answer):
+def add_test(file_loc, answer):
     tests.append(Test("./tests/"+file_loc, answer))
 
-def HasSucceeded(stdout, stderr, answer):
+def has_succeeded(stdout, stderr, answer):
     output = stdout.decode(ENCODER)
     error = stderr.decode(ENCODER)
     if error:
         return False
     return output.strip() == answer
 
-def RunTests():
+def run_tests():
     # Finds the executable regardless of platform
     exe = distutils.spawn.find_executable(BUILD)
     output = "End2End Test Results:\n"
-    haveFailed = 0
-    testCount = 0
+    have_failed = 0
+    test_count = 0
     for test in tests:
         try:
             process = subprocess.Popen([exe, test.file_loc], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             # After 3 minutes (180 seconds) the program will crash, to prevent eternal loops
             stdout, stderr = process.communicate(timeout=180) 
-            if HasSucceeded(stdout, stderr, test.answer):
+            if has_succeeded(stdout, stderr, test.answer):
                 output += "    > {}   -->   SUCCESS\n".format(test.file_loc.split('/')[-1])
             else:
                 output += "    > {}   -->   FAILED\n".format(test.file_loc.split('/')[-1])
-                haveFailed += 1
+                have_failed += 1
         except subprocess.TimeoutExpired:
             process.kill()
             stdout, stderr = process.communicate()
             output += "    > {}   -->   FAILED [TIME EXPIRED]\n".format(test.file_loc.split('/')[-1])
-            haveFailed += 1
-        testCount += 1
+            have_failed += 1
+        test_count += 1
 
-    output += "\nTotal: {} - Failed: {} - Succeeded: {}\n".format(testCount, haveFailed, testCount - haveFailed)
-    return (output, haveFailed>0)
+    output += "\nTotal: {} - Failed: {} - Succeeded: {}\n".format(test_count, have_failed, test_count - have_failed)
+    return (output, have_failed>0)
