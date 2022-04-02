671e338c40815290f3536ceb925089c8a341079a
Kane Petra <kane.petra@looplang.org>
2022-04-02 22:29:05 +0200

    tweak(tsp): sort methods & add explanations

--- src/compiler/compile/expression_index.rs
+++ src/compiler/compile/expression_index.rs
@@ -13,7 +13,7 @@ pub fn compile_expression_index(_compiler: &mut Compiler, _index: Index) -> Comp
     #[allow(clippy::single_match)]
     match _index.index.clone() {
         Expression::Call(call) => {
-            compile_expression_extension_method(_compiler, call, _index.left, true)
+            compile_expression_extension_method(_compiler, call, _index.left)
         }
         _ => compile_expression_index_internal(_compiler, _index.left, _index.index),
     }
@@ -52,7 +52,6 @@ pub fn compile_expression_extension_method(
     compiler: &mut Compiler,
     call: Call,
     left: Expression,
-    deeper: bool,
 ) -> CompilerResult {
     let method = match *call.identifier.clone() {
         Expression::Identifier(identifier) => identifier.value,
@@ -66,103 +65,149 @@ pub fn compile_expression_extension_method(
         return CompilerResult::Exception(CompilerException::UnknownExtensionMethod(method));
     }
 
-    /*
-        extension!(to_string),
-        extension!(to_int), // 1
-        extension!(add), // 2
-        extension!(remove), // 3
-        extension!(slice), // 4
-        extension!(length), // 5
-     */
-
-    let param_len = call.parameters.len();
-
-    if deeper {
-        compiler.compile_expression(left.clone());
-    }
+    compiler.compile_expression(left.clone());
 
     match method_id.unwrap() {
-        2 => {
-            compiler.add_to_current_function(" ~= ".to_string());
+        2 => transpile_extension_add(compiler, call),
+        3 => transpile_extension_remove(compiler, call, left),
+        4 => transpile_extension_slice(compiler, call),
+        5 => transpile_extension_length(compiler),
+        _ => CompilerResult::Exception(CompilerException::Unknown),
+    }
+}
 
-            let mut index = 0;
+fn transpile_extension_add(compiler: &mut Compiler, call: Call) -> CompilerResult {
+    /// Transpiles the extension method 'add'
+    ///
+    /// Take this Loop code:
+    /// ```loop
+    /// var array = [10, 20, 30];
+    /// array.add(40, 50);
+    /// ```
+    ///
+    /// And generates this D code:
+    /// ```d
+    /// auto var_array_0 = [10, 20, 30];
+    /// var_array_0 ~= [40, 50];
+    /// ```
+    compiler.add_to_current_function(" ~= ".to_string());
 
-            if call.parameters.len() > 1 {
-                compiler.add_to_current_function("[".to_string());
-            }
+    let mut index = 0;
 
-            for parameter in call.parameters.clone() {
-                let result = compiler.compile_expression(parameter);
-
-                #[allow(clippy::single_match)]
-                match &result {
-                    CompilerResult::Exception(_exception) => return result,
-                    _ => (),
-                }
-
-                index += 1;
-
-                if call.parameters.len() > 1 && call.parameters.len() != index {
-                    compiler.add_to_current_function(", ".to_string());
-                }
-            }
-
-            if call.parameters.len() > 1 {
-                compiler.add_to_current_function("]".to_string());
-            }
-        },
-        3 => {
-            compiler.add_to_current_function(" = ".to_string());
-
-            if deeper {
-                compiler.compile_expression(left);
-            }
-
-            compiler.add_to_current_function(".remove(".to_string());
-
-            let mut index = 0;
-            for parameter in call.parameters.clone() {
-                let result = compiler.compile_expression(parameter);
-
-                #[allow(clippy::single_match)]
-                match &result {
-                    CompilerResult::Exception(_exception) => return result,
-                    _ => (),
-                }
-
-                index += 1;
-
-                if call.parameters.len() > 1 && call.parameters.len() != index {
-                    compiler.add_to_current_function(", ".to_string());
-                }
-            }
-
-            compiler.add_to_current_function(")".to_string());
-        },
-        4 => {
-            compiler.add_to_current_function("[".to_string());
-
-            let start = call.parameters[0].clone();
-            let end = call.parameters[1].clone();
-
-            compiler.compile_expression(start);
-
-            compiler.add_to_current_function("..".to_string());
-
-            compiler.compile_expression(end);
-
-            compiler.add_to_current_function("]".to_string());
-        }
-        5 => {
-            compiler.add_to_current_function(".length".to_string());
-        }
-        _ => {}
+    if call.parameters.len() > 1 {
+        compiler.add_to_current_function("[".to_string());
     }
 
-    compiler.emit(
-        OpCode::CallExtension,
-        vec![method_id.unwrap() as u32, param_len as u32],
-    );
+    for parameter in call.parameters.clone() {
+        let result = compiler.compile_expression(parameter);
+
+        #[allow(clippy::single_match)]
+        match &result {
+            CompilerResult::Exception(_exception) => return result,
+            _ => (),
+        }
+
+        index += 1;
+
+        if call.parameters.len() > 1 && call.parameters.len() != index {
+            compiler.add_to_current_function(", ".to_string());
+        }
+    }
+
+    if call.parameters.len() > 1 {
+        compiler.add_to_current_function("]".to_string());
+    }
+
+    CompilerResult::Success
+}
+
+fn transpile_extension_remove(compiler: &mut Compiler, call: Call, left: Expression) -> CompilerResult {
+    /// Transpiles the extension method 'add'
+    ///
+    /// Take this Loop code:
+    /// ```loop
+    /// var array = [10, 20, 30];
+    /// array.remove(0, 1);
+    /// ```
+    ///
+    /// And generates this D code:
+    /// ```d
+    /// auto var_array_0 = [10, 20, 30];
+    /// var_array_0 = var_array_0.remove(0, 1);
+    /// ```
+    compiler.add_to_current_function(" = ".to_string());
+
+    compiler.compile_expression(left);
+
+    compiler.add_to_current_function(".remove(".to_string());
+
+    let mut index = 0;
+    for parameter in call.parameters.clone() {
+        let result = compiler.compile_expression(parameter);
+
+        #[allow(clippy::single_match)]
+        match &result {
+            CompilerResult::Exception(_exception) => return result,
+            _ => (),
+        }
+
+        index += 1;
+
+        if call.parameters.len() > 1 && call.parameters.len() != index {
+            compiler.add_to_current_function(", ".to_string());
+        }
+    }
+
+    compiler.add_to_current_function(")".to_string());
+
+    CompilerResult::Success
+}
+
+fn transpile_extension_slice(compiler: &mut Compiler, call: Call) -> CompilerResult {
+    /// Transpiles the extension method 'add'
+    ///
+    /// Take this Loop code:
+    /// ```loop
+    /// var array = [10, 20, 30];
+    /// var sliced = array.slice(0,2);
+    /// ```
+    ///
+    /// And generates this D code:
+    /// ```d
+    /// auto var_array_0 = [10, 20, 30];
+    /// auto sliced = var_array_0[0..2];
+    /// ```
+    compiler.add_to_current_function("[".to_string());
+
+    let start = call.parameters[0].clone();
+    let end = call.parameters[1].clone();
+
+    compiler.compile_expression(start);
+
+    compiler.add_to_current_function("..".to_string());
+
+    compiler.compile_expression(end);
+
+    compiler.add_to_current_function("]".to_string());
+
+    CompilerResult::Success
+}
+
+fn transpile_extension_length(compiler: &mut Compiler) -> CompilerResult {
+    /// Transpiles the extension method 'add'
+    ///
+    /// Take this Loop code:
+    /// ```loop
+    /// var array = [10, 20, 30];
+    /// var length = array.length()
+    /// ```
+    ///
+    /// And generates this D code:
+    /// ```d
+    /// auto var_array_0 = [10, 20, 30];
+    /// auto length = var_array_0.length;
+    /// ```
+    compiler.add_to_current_function(".length".to_string());
 
     CompilerResult::Success
 }
