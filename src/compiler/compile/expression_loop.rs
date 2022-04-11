2b45ddf4e81c4583bacd161c1d63d633e323a3bb
Kane Petra <kane.petra@looplang.org>
2022-04-11 12:06:01 +0200

    tweak(compiler): update code gen a bit for loops

--- src/compiler/compile/expression_loop.rs
+++ src/compiler/compile/expression_loop.rs
@@ -6,7 +6,7 @@ use crate::parser::types::{BaseTypes, Types};
 ///
 /// Take this example in Loop
 /// ```loop
-/// var x = 0
+/// x := 0
 /// for(x < 10) {
 ///     x = x + 1
 /// }
@@ -15,7 +15,7 @@ use crate::parser::types::{BaseTypes, Types};
 ///
 /// Will translate to this D code (excluding imports & main declaration)
 /// ```d
-/// auto x = 0;
+/// x := 0;
 /// while(x < 10) {
 ///     x = x + 1;
 /// }
@@ -45,7 +45,7 @@ pub fn compile_loop_expression(compiler: &mut Compiler, lp: Loop) -> CompilerRes
 ///
 /// Take this example in Loop
 /// ```loop
-/// var x = 0
+/// x := 0
 /// for(var n = 0..10) {
 ///     x = x + 1
 /// }
@@ -89,7 +89,7 @@ pub fn compile_loop_iterator_expression(
 ///
 /// Take this example in Loop
 /// ```loop
-/// var array = [10, 20, 30]
+/// array := [10, 20, 30]
 /// for(var value in array) {
 ///     println(value)
 /// }
@@ -97,19 +97,10 @@ pub fn compile_loop_iterator_expression(
 ///
 /// Will translate to this D code (excluding imports & main declaration)
 /// ```d
-/// auto var_array_0 = [10, 20, 30];
-/// auto var__iterator_array_1 = var_array_0;
-///
-/// int var__iterator_index_3 = 0;
-///
-/// auto var_value_2 = var__iterator_array_1[0];
-///
-/// while(var__iterator_index_3 < var__iterator_array_1.length) {
-///     writeln(var_value_2);var__iterator_index_3 += 1;
-///
-///     if(var__iterator_index_3 < var__iterator_array_1.length) {
-///         var_value_2 = var__iterator_array_1[var__iterator_index_3];
-///     }
+/// int[] var_array_0 = [10, 20, 30];
+/// foreach (var_value_1; var_array_0)
+/// {
+///     writeln(var_value_1);
 /// }
 /// ```
 pub fn compile_loop_array_iterator_expression(
@@ -118,50 +109,22 @@ pub fn compile_loop_array_iterator_expression(
 ) -> CompilerResult {
     compiler.enter_variable_scope();
 
-    // Put the array on the stack and assign it to a cache variable
-    let array = compiler.define_variable(
-        "_iterator_array".to_string(),
-        Types::Array(Box::from(Types::Basic(BaseTypes::Integer))),
-    );
-
-    // Array
-    compiler.add_to_current_function(format!("() {{ auto {} = ", array.transpile()));
-    compiler.compile_expression(*lp.array, false);
-    compiler.add_to_current_function(";".to_string());
+    // compiler.compile_expression(*lp.array, false);
+    // lp.identifier.value
 
     // Define the identifier variable, with the starting value of the array
     let var = compiler.define_variable(lp.identifier.value, Types::Basic(BaseTypes::Integer));
 
-    let index = compiler.define_variable(
-        "_iterator_index".to_string(),
-        Types::Basic(BaseTypes::Integer),
-    );
+    compiler.add_to_current_function(format!("foreach({}; ", var.transpile(),));
 
-    compiler.add_to_current_function(format!("int {} = 0;", index.transpile()));
-    compiler.add_to_current_function(format!(
-        "auto {} = {}[0];",
-        var.transpile(),
-        array.transpile()
-    ));
+    compiler.compile_expression(*lp.array, false);
 
-    compiler.add_to_current_function(format!(
-        "while({} < {}.length) {{ ",
-        index.transpile(),
-        array.transpile()
-    ));
+    compiler.add_to_current_function(") {".to_string());
 
     // Compile body and then increase the index
     let result = compiler.compile_loop_block(lp.body);
 
-    compiler.add_to_current_function(format!("{} += 1;", index.transpile()));
-    compiler.add_to_current_function(format!(
-        "if({} < {}.length) {{ {} = {}[{}]; }} }} }}();",
-        index.transpile(),
-        array.transpile(),
-        var.transpile(),
-        array.transpile(),
-        index.transpile()
-    ));
+    compiler.add_to_current_function("}".to_string());
 
     result
 }
