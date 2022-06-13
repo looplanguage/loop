7b83643fc1ecf8c609ff9ba409619a38cbe11e15
Kane Petra <kane.petra@looplang.org>
2022-06-13 12:27:43 +0200

    fix: overwriting methods

--- src/picasso/src/compiler/compile/statement_class.rs
+++ src/picasso/src/compiler/compile/statement_class.rs
@@ -22,15 +22,20 @@ pub fn compile_class_statement(compiler: &mut Compiler, class: Class) -> Compile
             let inherited_handles: Vec<_> = inherits.1.iter().collect();
 
             for inherited_handle in inherited_handles {
-                // TODO: overwrite old methods with same name
                 items.push(inherited_handle.clone());
             }
         }
     }
 
     for class_item in class.values {
+        let mut class_item = class_item.clone();
         let name = class_item.name.clone();
         let index = class_item.index;
+        let mut replace = None;
+
+        if let Some(it) = items.iter_mut().find(|item| item.name == name) {
+            replace = Some(it);
+        }
 
         match class_item.item {
             ClassItem::Property(property) => {
@@ -41,14 +46,19 @@ pub fn compile_class_statement(compiler: &mut Compiler, class: Class) -> Compile
                 if let CompilerResult::Success(_type) = node {
                     compiler.add_to_current_function(format!("{};", _type.transpile()));
 
-                    items.push(
-                        ClassItemType {
-                            name,
-                            index,
-                            class_item_type: _type,
-                            value: *property.expression.clone(),
-                        },
-                    )
+                    let mut new_item = ClassItemType {
+                        name,
+                        index,
+                        class_item_type: _type,
+                        value: *property.expression.clone(),
+                    };
+
+                    if let Some(replace) = replace {
+                        new_item.index = replace.index;
+                        *replace = new_item
+                    } else {
+                        items.push(new_item)
+                    }
                 } else {
                     return node;
                 };
@@ -70,42 +80,52 @@ pub fn compile_class_statement(compiler: &mut Compiler, class: Class) -> Compile
                     },
                 );
 
-                items.push(
-                    ClassItemType {
+                let mut new_item = ClassItemType {
+                    name: name.clone(),
+                    index,
+                    class_item_type: Types::Function(FunctionType {
+                        return_type: Box::new(method.return_type.clone()),
+                        parameter_types: method
+                            .arguments
+                            .clone()
+                            .into_iter()
+                            .map(|v| v._type)
+                            .collect(),
+                        reference: "".to_string(),
+                    }),
+                    value: Expression::Function(Function {
                         name: name.clone(),
-                        index,
-                        class_item_type: Types::Function(FunctionType {
-                            return_type: Box::new(method.return_type.clone()),
-                            parameter_types: method
-                                .arguments
-                                .clone()
-                                .into_iter()
-                                .map(|v| v._type)
-                                .collect(),
-                            reference: "".to_string(),
-                        }),
-                        value: Expression::Function(Function {
-                            name: name.clone(),
-                            parameters: method.arguments.clone(),
-                            body: method.body.clone(),
-                            predefined_type: Some(method.return_type.clone()),
-                        }),
-                    },
-                );
+                        parameters: method.arguments.clone(),
+                        body: method.body.clone(),
+                        predefined_type: Some(method.return_type.clone()),
+                    })
+                };
+
+                if let Some(replace) = replace {
+                    new_item.index = replace.index;
+                    *replace = new_item
+                } else {
+                    items.push(new_item)
+                }
 
                 compiler.add_to_current_function(format!("{};", method.return_type.transpile()));
             }
             ClassItem::Lazy(lazy) => {
                 let value = Expression::Integer(Integer { value: 0 });
 
-                items.push(
-                    ClassItemType {
-                        name,
-                        index,
-                        class_item_type: lazy.clone(),
-                        value,
-                    },
-                );
+                let mut new_item = ClassItemType {
+                    name,
+                    index,
+                    class_item_type: lazy.clone(),
+                    value,
+                };
+
+                if let Some(replace) = replace {
+                    new_item.index = replace.index;
+                    *replace = new_item
+                } else {
+                    items.push(new_item)
+                }
 
                 compiler.add_to_current_function(format!("{};", lazy.transpile()))
             }
