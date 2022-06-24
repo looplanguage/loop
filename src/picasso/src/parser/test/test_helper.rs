9c6f7863f8d94fb2a90337346bd2a3352f8ad029
Kane Petra <kane.petra@looplang.org>
2022-06-24 20:31:26 +0200

    fix: test_helper didn't contain new field

--- src/picasso/src/parser/test/test_helper.rs
+++ src/picasso/src/parser/test/test_helper.rs
@@ -80,6 +80,7 @@ pub mod test_helper {
             body: Block { statements },
             name: "".to_string(),
             predefined_type: None,
+            public: false,
         })
     }
 
