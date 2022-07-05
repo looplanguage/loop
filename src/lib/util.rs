1097f96cbef2d3bf08960218682cd93788f6c4c0
WouterPennings <wouterpennings@gmail.com>
2022-07-05 14:46:52 +0200

    tweak: removed the stylua depedency and no longer formatting lua ouput

--- src/lib/util.rs
+++ src/lib/util.rs
@@ -5,7 +5,6 @@ use std::env;
 use std::io::Read;
 use std::path::Path;
 use std::process::{exit, ExitCode};
-use stylua_lib::{Config, OutputVerification};
 use vinci::types::ValueType;
 
 pub fn print_valuetype(value_type: ValueType) {
@@ -78,7 +77,7 @@ pub fn run_file(path: String) -> Result<(), ExitCode> {
     }
 
     if CONFIG.lua_output {
-        save_to_file(format!("{}.lua", path), format_lua(lua_code));
+        save_to_file(format!("{}.lua", path), lua_code);
     }
 
     let result = backend.run(ast);
@@ -107,19 +106,4 @@ fn save_to_file(file_name: String, content: String) {
             std::process::exit(1);
         }
     }
-}
-
-fn format_lua(lua: String) -> String {
-    let res = stylua_lib::format_code(
-        lua.as_str(),
-        Config::default(),
-        None,
-        OutputVerification::None,
-    );
-    if let Ok(formatted) = res {
-        return formatted;
-    }
-
-    println!("WARNING: COULD NOT FORMAT LUA CODE");
-    lua
 }
